846dd572242891a070514ebfbd47aec7fa9df65d
Frostz4 <nuraamalbek@gmail.com>
2024-06-03 11:00:40 +0500

    ngoasnf

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -6,6 +6,13 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Cache-Control", "public, max-age=31536000")
+	w.Header().Add("Cache-Control", "public")
+	w.Header().Add("Cache-Control", "max-age=31536000")
+	w.Header().Del("Cache-Control")
+	w.Header().Get("Cache-Control")
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"name":"Alex"}`))
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -18,6 +25,12 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		w.WriteHeader(405)
+		w.Write([]byte("GET-Метод запрещен!"))
+		return
+	}
 	w.Write([]byte("Форма для создания новой заметки..."))
 }
 
